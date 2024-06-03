4585551ae55886e6c9963e1364bf3c0364af00cb
Peppecoding <83593668+Peppecoding@users.noreply.github.com>
2024-06-03 17:12:43 +0100

    Add files via upload

---
+++ Day 1/Band-name-generator.py
@@ -0,0 +1,9 @@
+# Write your code below this line 👇
+print("Welcome to the Band Name Generator.")
+print()  # This empty print statement adds a space
+
+city = input("What is the city you grew up in?\n") 
+pet = input("What is your pet's name?\n")
+print("Your band name could be " + city + " " + pet)
+      
+# this excise highlight the use of VARIABLES, STRINGS AND CONCATENATE"
